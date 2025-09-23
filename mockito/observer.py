14bd6224662456b0910c4f7a2f734e28abd45748
Christian Mancini <mancinichristian00@gmail.com>
2025-09-23 23:52:18 +0200

    Added Observer abstract class

---
+++ mockito/observer.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+
+
+class Observer(ABC):
+    @abstractmethod
+    def update(self, subject: Subject) -> None: pass
+
+
+class Subject(ABC):
+
+    @abstractmethod
+    def attach(self, observer: Observer) -> None: pass
+
+    @abstractmethod
+    def detatch(self, observer: Observer) -> None: pass
+
+    @abstractmethod
+    def notify(self) -> None: pass
