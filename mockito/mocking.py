0461a322dff5c58ddb453430d0909873e7b88983
Christian Mancini <mancinichristian00@gmail.com>
2025-09-27 15:22:49 +0200

    Removed overrides and fixed typos

--- mockito/mocking.py
+++ mockito/mocking.py
@@ -32,7 +32,7 @@ __tracebackhide__ = operator.methodcaller(
     invocation.InvocationError
 )
 
-from typing import Deque, List, Union, override
+from typing import Deque, List, Union
 
 from .observer import Subject, Observer
 
@@ -70,17 +70,14 @@ class Mock(Subject):
 
         self._observers: List[Observer] = []
 
-    @override
     def attach(self, observer: Observer) -> None:
         self._observers.append(observer)
         print(f"{self.spec}: attached to {observer}")
 
-    @override
-    def detatch(self, observer: Observer) -> None:
+    def detach(self, observer: Observer) -> None:
         self._observers.remove(observer)
         print(f"{self.spec}: detached to {observer}")
 
-    @override
     def notify(self) -> None:
         for observer in self._observers:
             print(f"{self.spec}: notifying observers")
