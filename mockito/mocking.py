f3c874d487e19da400ed8817f33993ebb62f5a2c
Christian Mancini <mancinichristian00@gmail.com>
2025-10-05 10:13:17 +0200

    Removed mock repr

--- mockito/mocking.py
+++ mockito/mocking.py
@@ -259,11 +259,6 @@ class Mock(Subject):
                 )
             )
 
-    def __repr__(self):
-        if self.spec:
-            return f"Mock of class: {self.spec.__name__}"
-        return "Dummy Mock"
-
 
 class _OMITTED(object):
     def __repr__(self):
